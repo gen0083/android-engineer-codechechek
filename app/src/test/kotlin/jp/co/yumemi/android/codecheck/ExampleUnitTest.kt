8c092538f9c7300ed7b26279b9c12c0a40cf1122
Gen <archiherewego@gmail.com>
2024-06-01 23:04:09 +0900

    test: コーディングのためのテストを作成
    github apiのレスポンス形式を確認するためのテスト

--- app/src/test/kotlin/jp/co/yumemi/android/codecheck/ExampleUnitTest.kt
+++ app/src/test/kotlin/jp/co/yumemi/android/codecheck/ExampleUnitTest.kt
@@ -1,6 +1,14 @@
 package jp.co.yumemi.android.codecheck
 
-import org.junit.Assert.assertEquals
+import io.ktor.client.HttpClient
+import io.ktor.client.call.receive
+import io.ktor.client.engine.android.Android
+import io.ktor.client.request.get
+import io.ktor.client.request.header
+import io.ktor.client.request.parameter
+import io.ktor.client.statement.HttpResponse
+import kotlinx.coroutines.runBlocking
+import kotlinx.serialization.json.Json
 import org.junit.Test
 
 /**
@@ -9,8 +17,18 @@ import org.junit.Test
  * See [testing documentation](http://d.android.com/tools/testing).
  */
 class ExampleUnitTest {
+    private val client = HttpClient(Android)
+    private val json = Json {
+        ignoreUnknownKeys = true
+    }
     @Test
-    fun addition_isCorrect() {
-        assertEquals(4, 2 + 2)
+    fun addition_isCorrect() = runBlocking {
+        val response: HttpResponse = client.get("https://api.github.com/search/repositories") {
+            header("Accept", "application/vnd.github.v3+json")
+            parameter("q", "asdfghjkk")
+        }
+
+        val jsonBody = json.parseToJsonElement(response.receive<String>())
+        println(jsonBody)
     }
 }
