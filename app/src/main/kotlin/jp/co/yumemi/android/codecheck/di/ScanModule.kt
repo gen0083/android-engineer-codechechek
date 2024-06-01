eedf2256086e6d30328566a9e17271b3f3921032
Gen <archiherewego@gmail.com>
2024-06-02 07:30:45 +0900

    add: Applicationクラスを定義しstartKoinでkoin導入

---
+++ app/src/main/kotlin/jp/co/yumemi/android/codecheck/di/ScanModule.kt
@@ -0,0 +1,8 @@
+package jp.co.yumemi.android.codecheck.di
+
+import org.koin.core.annotation.ComponentScan
+import org.koin.core.annotation.Module
+
+@Module
+@ComponentScan("jp.co.yumemi.android.codecheck")
+class ScanModule
