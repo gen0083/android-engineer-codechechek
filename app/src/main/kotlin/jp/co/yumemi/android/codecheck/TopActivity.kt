b9aa815c71e7aa14230c10d22e3586abd9e5f9d1
Gen <archiherewego@gmail.com>
2024-06-02 23:20:39 +0900

    refactor: 検索日時を表示するように変更
    Log.dで出すだけではわからないので、検索日時を表示するように変更した

--- app/src/main/kotlin/jp/co/yumemi/android/codecheck/TopActivity.kt
+++ app/src/main/kotlin/jp/co/yumemi/android/codecheck/TopActivity.kt
@@ -4,11 +4,6 @@
 package jp.co.yumemi.android.codecheck
 
 import androidx.appcompat.app.AppCompatActivity
-import java.util.Date
 
 class TopActivity : AppCompatActivity(R.layout.activity_top) {
-
-    companion object {
-        lateinit var lastSearchDate: Date
-    }
 }
