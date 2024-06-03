414bc3c179c8814bebe34ac09a652abc0e48c36a
Gen <archiherewego@gmail.com>
2024-06-04 00:09:57 +0900

    delete: Composeにしたことによって使わなくなったので削除
    そのうち依存も消せる

--- app/src/main/kotlin/jp/co/yumemi/android/codecheck/adapter/OnItemClickListener.kt
+++
@@ -1,7 +0,0 @@
-package jp.co.yumemi.android.codecheck.adapter
-
-import jp.co.yumemi.android.codecheck.api.RepositoryInfo
-
-interface OnItemClickListener {
-    fun itemClick(item: RepositoryInfo)
-}
