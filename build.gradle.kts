105326028d4cc7961f1a7fed977824318016969c
Gen <archiherewego@gmail.com>
2024-06-05 00:55:42 +0900

    add: KMP導入の準備第1段階
    KMPモジュールを選択すればとりあえず入るっぽい
    しかし
    libs.plugins.androidLibraryとか勝手にいれるくせにlibs.versions.tomlは更新してくれないという・・・なんやそれ
    というわけで、pluginを定義してあらかじめapply falseしておくのを先にやっておく

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,9 +1,11 @@
 plugins {
     alias(libs.plugins.androidApplication) apply false
+    alias(libs.plugins.androidLibrary) apply false
     alias(libs.plugins.kotlinAndroid) apply false
     alias(libs.plugins.kotlinSerialization) apply false
     alias(libs.plugins.ksp) apply false
     alias(libs.plugins.compose) apply false
+    alias(libs.plugins.kotlinMultiplatform) apply false
     id("androidx.navigation.safeargs") version "2.5.3" apply false
 }
 
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -13,4 +13,6 @@ dependencyResolutionManagement {
     }
 }
 rootProject.name = "Android Engineer CodeCheck"
-include(":app")
+include(
+    ":app",
+)
