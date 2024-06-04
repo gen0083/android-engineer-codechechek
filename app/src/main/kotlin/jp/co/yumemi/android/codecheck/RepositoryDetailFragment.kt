2f795e91eccd7b9d319d0c209c110068a42e2484
Gen <archiherewego@gmail.com>
2024-06-04 10:57:37 +0900

    update: RepositoryDetailをCompose化
    ここはあんまり変わらないけども・・・
    それより作りに問題が出てきそうな感じ・・・

--- app/src/main/kotlin/jp/co/yumemi/android/codecheck/RepositoryDetailFragment.kt
+++ app/src/main/kotlin/jp/co/yumemi/android/codecheck/RepositoryDetailFragment.kt
@@ -4,36 +4,38 @@
 package jp.co.yumemi.android.codecheck
 
 import android.os.Bundle
+import android.view.LayoutInflater
 import android.view.View
+import android.view.ViewGroup
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.ui.platform.ComposeView
+import androidx.compose.ui.platform.ViewCompositionStrategy
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.navArgs
-import coil.load
 import jp.co.yumemi.android.codecheck.api.SearchClient
-import jp.co.yumemi.android.codecheck.databinding.FragmentDetailBinding
+import jp.co.yumemi.android.codecheck.ui.RepositoryDetailContent
 import org.koin.android.ext.android.inject
 
-class RepositoryDetailFragment : Fragment(R.layout.fragment_detail) {
+class RepositoryDetailFragment : Fragment() {
 
     private val args: RepositoryDetailFragmentArgs by navArgs()
-
-    private lateinit var binding: FragmentDetailBinding
     private val searchClient by inject<SearchClient>()
 
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
-
-        binding = FragmentDetailBinding.bind(view)
-
-        val item = args.item
-
-        binding.ownerIconView.load(item.owner.ownerIconUrl)
-        binding.nameView.text = item.name
-        binding.languageView.text = getString(R.string.written_language, item.language ?: "unknown")
-        binding.starsView.text = getString(R.string.count_stars, item.stargazersCount)
-        binding.watchersView.text = getString(R.string.count_watchers, item.watchersCount)
-        binding.forksView.text = getString(R.string.count_forks, item.forksCount)
-        binding.openIssuesView.text = getString(R.string.count_open_issues, item.openIssuesCount)
-        binding.searchDate.text =
-            getString(R.string.search_text, searchClient.lastSearchDate.toString())
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?,
+    ): View {
+        return ComposeView(requireContext()).apply {
+            setViewCompositionStrategy(ViewCompositionStrategy.DisposeOnViewTreeLifecycleDestroyed)
+            setContent {
+                MaterialTheme {
+                    RepositoryDetailContent(
+                        info = args.item,
+                        lastSearchDate = searchClient.lastSearchDate.toString(),
+                    )
+                }
+            }
+        }
     }
 }
