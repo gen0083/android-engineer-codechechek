0849939562f33ff142fed16a3f55e7472289d57b
Gen <archiherewego@gmail.com>
2024-06-07 20:35:47 +0900

    change: ScreenModel経由でsearchClientにアクセスするよう変更
    data classなのでこれ以上コンストラクタで渡すわけにもいかず・・・

--- app/src/main/kotlin/jp/co/yumemi/android/codecheck/ui/RepositoryDetailScreen.kt
+++ app/src/main/kotlin/jp/co/yumemi/android/codecheck/ui/RepositoryDetailScreen.kt
@@ -2,15 +2,20 @@ package jp.co.yumemi.android.codecheck.ui
 
 import androidx.compose.runtime.Composable
 import cafe.adriel.voyager.core.screen.Screen
+import cafe.adriel.voyager.koin.koinScreenModel
 import jp.co.yumemi.android.codecheck.api.RepositoryInfo
 
-data class RepositoryDetailScreen(private val info: RepositoryInfo) : Screen {
+data class RepositoryDetailScreen(
+    private val info: RepositoryInfo,
+) : Screen {
     @Composable
     override fun Content() {
+        val screenModel: RepositoryDetailScreenModel =
+            koinScreenModel<RepositoryDetailScreenModel>()
+
         RepositoryDetailContent(
-            // TODO: replace real data
             info = info,
-            lastSearchDate = "",
+            lastSearchDate = screenModel.getLastSearchTime(),
         )
     }
 }
