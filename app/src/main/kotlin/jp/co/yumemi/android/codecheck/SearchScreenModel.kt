db4e197e0cc7c005577ddf83283c185407d9a208
Gen <archiherewego@gmail.com>
2024-06-07 19:07:46 +0900

    change: StateScreenModelに書き換え
    ScreenModelからはmutableState、外部からは読み取り専用のstateとして公開されるので、ExplicitBackingFields使わなくてもいいな

--- app/src/main/kotlin/jp/co/yumemi/android/codecheck/SearchScreenModel.kt
+++ app/src/main/kotlin/jp/co/yumemi/android/codecheck/SearchScreenModel.kt
@@ -3,34 +3,35 @@
  */
 package jp.co.yumemi.android.codecheck
 
-import androidx.compose.runtime.State
-import androidx.compose.runtime.mutableStateOf
-import cafe.adriel.voyager.core.model.ScreenModel
+import cafe.adriel.voyager.core.model.StateScreenModel
 import cafe.adriel.voyager.core.model.screenModelScope
 import jp.co.yumemi.android.codecheck.api.RepositoryInfo
 import jp.co.yumemi.android.codecheck.api.SearchClient
-import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.update
 import kotlinx.coroutines.launch
 import org.koin.core.annotation.Singleton
 
 @Singleton
 class SearchScreenModel(
     private val searchClient: SearchClient,
-) : ScreenModel {
-    val list: StateFlow<List<RepositoryInfo>>
-        field = kotlinx.coroutines.flow.MutableStateFlow<List<RepositoryInfo>>(
-            kotlin.collections.listOf(),
-        )
-    val isLoading: State<Boolean>
-        field = androidx.compose.runtime.mutableStateOf(false)
+) : StateScreenModel<SearchScreenModel.State>(State()) {
+
+    data class State(
+        val list: List<RepositoryInfo> = listOf(),
+        val isLoading: Boolean = false,
+    )
 
     // 検索結果
     fun searchResults(inputText: String) {
         if (inputText.isBlank()) return
         screenModelScope.launch {
-            isLoading.value = true
-            list.value = searchClient.searchRepository(inputText)
-            isLoading.value = false
+            mutableState.update { it.copy(isLoading = true) }
+            mutableState.update {
+                it.copy(
+                    list = searchClient.searchRepository(inputText),
+                    isLoading = false,
+                )
+            }
         }
     }
 }
