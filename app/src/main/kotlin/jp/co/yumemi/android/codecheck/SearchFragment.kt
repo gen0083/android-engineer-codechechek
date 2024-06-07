2d483b877e25723a0439d9741ed90ea8080564a4
Gen <archiherewego@gmail.com>
2024-06-07 19:02:50 +0900

    remove: FragmentからViewModelを削除
    最終的にはFragment自体消えるからいっそ消してしまってもいいのだけど、消すのは最後にしよう

--- app/src/main/kotlin/jp/co/yumemi/android/codecheck/SearchFragment.kt
+++ app/src/main/kotlin/jp/co/yumemi/android/codecheck/SearchFragment.kt
@@ -8,18 +8,14 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.runtime.collectAsState
-import androidx.compose.runtime.getValue
 import androidx.compose.ui.platform.ComposeView
 import androidx.compose.ui.platform.ViewCompositionStrategy
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import jp.co.yumemi.android.codecheck.api.RepositoryInfo
 import jp.co.yumemi.android.codecheck.ui.SearchContent
-import org.koin.androidx.viewmodel.ext.android.viewModel
 
 class SearchFragment : Fragment() {
-    private val viewModel: SearchViewModel by viewModel()
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -29,13 +25,11 @@ class SearchFragment : Fragment() {
         return ComposeView(requireContext()).apply {
             setViewCompositionStrategy(ViewCompositionStrategy.DisposeOnViewTreeLifecycleDestroyed)
             setContent {
-                val list by viewModel.list.collectAsState()
-                val isLoading by viewModel.isLoading
                 MaterialTheme {
                     SearchContent(
-                        list = list,
-                        isLoading = isLoading,
-                        onTextChanged = viewModel::searchResults,
+                        list = listOf(),
+                        isLoading = false,
+                        onTextChanged = {},
                         onNavigate = ::gotoRepositoryFragment,
                     )
                 }
