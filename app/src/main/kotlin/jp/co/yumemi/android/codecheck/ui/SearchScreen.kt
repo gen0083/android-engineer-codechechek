f084e2ebc0d51cc87203b5e8d71257062048ae11
Gen <archiherewego@gmail.com>
2024-06-07 18:45:43 +0900

    add: SearchScreenにnavigatorを実装
    まだ移動はできないけどね・・・
    contents側にnavigatorが持ってこれるのはいいなあと思った
    compose-navigationだと今やってるみたいにNavigateする処理を渡すという感じで作らないといけないので直接かけるのはいい
    それがいいか悪いかは別としてだけど

--- app/src/main/kotlin/jp/co/yumemi/android/codecheck/ui/SearchScreen.kt
+++ app/src/main/kotlin/jp/co/yumemi/android/codecheck/ui/SearchScreen.kt
@@ -2,16 +2,20 @@ package jp.co.yumemi.android.codecheck.ui
 
 import androidx.compose.runtime.Composable
 import cafe.adriel.voyager.core.screen.Screen
+import cafe.adriel.voyager.navigator.LocalNavigator
+import cafe.adriel.voyager.navigator.currentOrThrow
 
 class SearchScreen : Screen {
     @Composable
     override fun Content() {
+        val navigator = LocalNavigator.currentOrThrow
+
         SearchContent(
             // TODO: replace real data
             list = listOf(),
             isLoading = false,
             onTextChanged = {},
-            onNavigate = {},
+            onNavigate = { navigator.push(RepositoryDetailScreen(it)) },
         )
     }
 }
