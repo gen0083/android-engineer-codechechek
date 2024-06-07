7cc79e755800a13c238ee8fadb9321f915741c47
Gen <archiherewego@gmail.com>
2024-06-07 20:02:09 +0900

    change: ScreenModelを参照するように変更
    シンプルな構成だからシンプルになっているのか、voyagerだからシンプルになっているのか
    とにかく今のところ感触はかなり良い

--- app/src/main/kotlin/jp/co/yumemi/android/codecheck/ui/SearchScreen.kt
+++ app/src/main/kotlin/jp/co/yumemi/android/codecheck/ui/SearchScreen.kt
@@ -1,20 +1,25 @@
 package jp.co.yumemi.android.codecheck.ui
 
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
 import cafe.adriel.voyager.core.screen.Screen
+import cafe.adriel.voyager.koin.koinScreenModel
 import cafe.adriel.voyager.navigator.LocalNavigator
 import cafe.adriel.voyager.navigator.currentOrThrow
+import jp.co.yumemi.android.codecheck.SearchScreenModel
 
 class SearchScreen : Screen {
     @Composable
     override fun Content() {
         val navigator = LocalNavigator.currentOrThrow
+        val screenModel = koinScreenModel<SearchScreenModel>()
+        val state by screenModel.state.collectAsState()
 
         SearchContent(
-            // TODO: replace real data
-            list = listOf(),
-            isLoading = false,
-            onTextChanged = {},
+            list = state.list,
+            isLoading = state.isLoading,
+            onTextChanged = { screenModel.searchResults(it) },
             onNavigate = { navigator.push(RepositoryDetailScreen(it)) },
         )
     }
