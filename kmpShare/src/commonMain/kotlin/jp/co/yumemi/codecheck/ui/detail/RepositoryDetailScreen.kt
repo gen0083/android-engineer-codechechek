f43e8507382ae25ce5e07583638b2539d275b68f
Gen <archiherewego@gmail.com>
2024-06-09 22:39:37 +0900

    add: TopAppBarを表示
    Screenごとに実装しているから全然DRYじゃないね・・・
    Appに配置しているNavigatorにScaffoldを適用すれば共通利用できるが、それだと表示中のScreenに応じてNavigationIconを出し分けることができなかった
    たぶんAppはRecompositionが走ってないのではないかなぁ
    やり方が間違っている可能性のほうが高そうだが・・・
    ひとまず力技で押し通る

--- kmpShare/src/commonMain/kotlin/jp/co/yumemi/codecheck/ui/detail/RepositoryDetailScreen.kt
+++ kmpShare/src/commonMain/kotlin/jp/co/yumemi/codecheck/ui/detail/RepositoryDetailScreen.kt
@@ -1,25 +1,66 @@
 package jp.co.yumemi.codecheck.ui.detail
 
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.filled.ArrowBack
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import cafe.adriel.voyager.core.model.rememberScreenModel
 import cafe.adriel.voyager.core.screen.Screen
+import cafe.adriel.voyager.navigator.LocalNavigator
+import cafe.adriel.voyager.navigator.currentOrThrow
 import jp.co.yumemi.codecheck.api.RepositoryInfo
+import jp.co.yumemi.codecheck.resources.Res
+import jp.co.yumemi.codecheck.resources.app_name
+import org.jetbrains.compose.resources.stringResource
 import org.koin.core.component.KoinComponent
 import org.koin.core.component.inject
 
 data class RepositoryDetailScreen(
     private val info: RepositoryInfo,
 ) : Screen, KoinComponent {
+    @OptIn(ExperimentalMaterial3Api::class)
     @Composable
     override fun Content() {
         val screenModel: RepositoryDetailScreenModel =
             rememberScreenModel<RepositoryDetailScreenModel> {
                 inject<RepositoryDetailScreenModel>().value
             }
+        val navigator = LocalNavigator.currentOrThrow
 
-        RepositoryDetailContent(
-            info = info,
-            lastSearchDate = screenModel.getLastSearchTime(),
-        )
+        Scaffold(
+            topBar = {
+                TopAppBar(
+                    title = {
+                        Text(text = stringResource(Res.string.app_name))
+                    },
+                    navigationIcon = {
+                        IconButton(
+                            onClick = { navigator.pop() },
+                            content = {
+                                Icon(
+                                    imageVector = Icons.AutoMirrored.Default.ArrowBack,
+                                    contentDescription = "back",
+                                )
+                            },
+                        )
+                    },
+                    colors = TopAppBarDefaults.topAppBarColors(),
+                )
+            },
+        ) {
+            RepositoryDetailContent(
+                info = info,
+                lastSearchDate = screenModel.getLastSearchTime(),
+                modifier = Modifier.padding(it),
+            )
+        }
     }
 }
--- kmpShare/src/commonMain/kotlin/jp/co/yumemi/codecheck/ui/search/SearchScreen.kt
+++ kmpShare/src/commonMain/kotlin/jp/co/yumemi/codecheck/ui/search/SearchScreen.kt
@@ -1,17 +1,31 @@
 package jp.co.yumemi.codecheck.ui.search
 
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.filled.ArrowBack
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.ui.Modifier
 import cafe.adriel.voyager.core.model.rememberScreenModel
 import cafe.adriel.voyager.core.screen.Screen
 import cafe.adriel.voyager.navigator.LocalNavigator
 import cafe.adriel.voyager.navigator.currentOrThrow
+import jp.co.yumemi.codecheck.resources.Res
+import jp.co.yumemi.codecheck.resources.app_name
 import jp.co.yumemi.codecheck.ui.detail.RepositoryDetailScreen
+import org.jetbrains.compose.resources.stringResource
 import org.koin.core.component.KoinComponent
 import org.koin.core.component.inject
 
 class SearchScreen : Screen, KoinComponent {
+    @OptIn(ExperimentalMaterial3Api::class)
     @Composable
     override fun Content() {
         val navigator = LocalNavigator.currentOrThrow
@@ -20,11 +34,35 @@ class SearchScreen : Screen, KoinComponent {
         }
         val state by screenModel.state.collectAsState()
 
-        SearchContent(
-            list = state.list,
-            isLoading = state.isLoading,
-            onTextChanged = { screenModel.searchResults(it) },
-            onNavigate = { navigator.push(RepositoryDetailScreen(it)) },
-        )
+        Scaffold(
+            topBar = {
+                TopAppBar(
+                    title = {
+                        Text(text = stringResource(Res.string.app_name))
+                    },
+                    navigationIcon = {
+                        if (navigator.parent != null) {
+                            IconButton(
+                                onClick = { navigator.pop() },
+                                content = {
+                                    Icon(
+                                        imageVector = Icons.AutoMirrored.Default.ArrowBack,
+                                        contentDescription = "back",
+                                    )
+                                },
+                            )
+                        }
+                    },
+                )
+            },
+        ) {
+            SearchContent(
+                list = state.list,
+                isLoading = state.isLoading,
+                onTextChanged = { screenModel.searchResults(it) },
+                onNavigate = { navigator.push(RepositoryDetailScreen(it)) },
+                modifier = Modifier.padding(paddingValues = it),
+            )
+        }
     }
 }
