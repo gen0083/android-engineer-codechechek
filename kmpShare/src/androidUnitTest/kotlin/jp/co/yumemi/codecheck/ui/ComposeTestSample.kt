194621f285a8ef4680bcde861f4bb0aefcfd2bdb
Gen <archiherewego@gmail.com>
2024-06-11 12:46:33 +0900

    add: ログ出力の方法を確認
    printToLogが何も出ないなあと思っていろいろ調べて、log4jの設定がいるのかと遠回りしながらたどり着いたのはLog.dだった
    Log.dをrobolectricで切り替えないといけないのはstackoverflowでわかった
    https://stackoverflow.com/questions/10219915/where-is-log-output-written-to-when-using-robolectric-roboguice

--- kmpShare/src/androidUnitTest/kotlin/jp/co/yumemi/codecheck/ui/ComposeTestSample.kt
+++ kmpShare/src/androidUnitTest/kotlin/jp/co/yumemi/codecheck/ui/ComposeTestSample.kt
@@ -1,5 +1,6 @@
 package jp.co.yumemi.codecheck.ui
 
+import android.util.Log
 import androidx.compose.ui.test.ExperimentalTestApi
 import androidx.compose.ui.test.hasTestTag
 import androidx.compose.ui.test.hasText
@@ -7,6 +8,7 @@ import androidx.compose.ui.test.junit4.createComposeRule
 import androidx.compose.ui.test.onNodeWithTag
 import androidx.compose.ui.test.onRoot
 import androidx.compose.ui.test.performTextInput
+import androidx.compose.ui.test.printToLog
 import com.github.takahirom.roborazzi.captureRoboImage
 import jp.co.yumemi.codecheck.App
 import kotlinx.coroutines.test.runTest
@@ -15,6 +17,7 @@ import org.junit.Test
 import org.junit.runner.RunWith
 import org.robolectric.RobolectricTestRunner
 import org.robolectric.annotation.GraphicsMode
+import org.robolectric.shadows.ShadowLog
 
 @RunWith(RobolectricTestRunner::class)
 @GraphicsMode(GraphicsMode.Mode.NATIVE)
@@ -36,6 +39,11 @@ class ComposeTestSample {
     @OptIn(ExperimentalTestApi::class)
     @Test
     fun inputTest() = runTest {
+        // printlnは普通にログに出る
+        println("test start")
+        // Log.dはShadowLog.stream = System.outに切り替えしないと何も出ない
+        ShadowLog.stream = System.out
+        Log.d("a", "Log.d print message")
         composeRule.setContent { App() }
         composeRule.awaitIdle()
         // テキスト入力しただけでrecompositionは発生しない
@@ -47,6 +55,10 @@ class ComposeTestSample {
             hasTestTag("result"),
             5_000,
         )
+        // Roborazziが画像で出してくれるけども、テキストで確認したい場合はprintToLogが使える
+        // ただし中身はLog.dでログに出力しているのでRobolectricを使う場合はログの切り替えをしないと何も出力されない
+        // 引数に渡すタグはLog.dで使われるタグ
+        composeRule.onRoot().printToLog("test")
         // キャプチャを取る→すでに画像が存在している場合、画像に変化がある場合にテストに失敗する
         composeRule.onRoot()
             .captureRoboImage("build/compose.png")
