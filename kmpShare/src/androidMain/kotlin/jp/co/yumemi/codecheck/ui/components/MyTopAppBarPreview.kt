8d2632dd7aef51406a758547b0f102a0cec22af6
Gen <archiherewego@gmail.com>
2024-06-14 01:51:37 +0900

    add: Light-Darkのプレビューを追加
    Light Darkでコンテンツが読めるかどうかをチェックするテストはどう書けばいいんだろうなあ・・・

--- kmpShare/src/androidMain/kotlin/jp/co/yumemi/codecheck/ui/components/MyTopAppBarPreview.kt
+++ kmpShare/src/androidMain/kotlin/jp/co/yumemi/codecheck/ui/components/MyTopAppBarPreview.kt
@@ -2,9 +2,11 @@ package jp.co.yumemi.codecheck.ui.components
 
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.tooling.preview.PreviewLightDark
 import jp.co.yumemi.codecheck.ui.theme.AppTheme
 
 @Preview
+@PreviewLightDark
 @Composable
 private fun MyTopAppBarPreview() {
     AppTheme {
