bb8cbe92e0a168c31699b4a9b3ac334c67eccdeb
Gen <archiherewego@gmail.com>
2024-06-17 21:44:15 +0900

    add: RepositoryListScreenModelを定義
    ユーザー名が同じなら同一のScreenModelが取ってこれるようにした（つもり）

--- kmpShare/src/commonMain/kotlin/jp/co/yumemi/codecheck/ui/detail/RepositoryListScreen.kt
+++ kmpShare/src/commonMain/kotlin/jp/co/yumemi/codecheck/ui/detail/RepositoryListScreen.kt
@@ -5,26 +5,42 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.navigationBarsPadding
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.statusBarsPadding
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Close
+import androidx.compose.material3.Button
+import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
+import cafe.adriel.voyager.core.model.rememberNavigatorScreenModel
 import cafe.adriel.voyager.core.screen.Screen
+import cafe.adriel.voyager.navigator.LocalNavigator
 import cafe.adriel.voyager.navigator.bottomSheet.LocalBottomSheetNavigator
+import cafe.adriel.voyager.navigator.currentOrThrow
+import org.koin.core.component.KoinComponent
+import org.koin.core.component.inject
+import org.koin.core.parameter.parametersOf
 
 data class RepositoryListScreen(
     val userName: String,
-) : Screen {
+) : Screen, KoinComponent {
     @ExperimentalMaterial3Api
     @Composable
     override fun Content() {
+        val navigator = LocalNavigator.currentOrThrow
         val bottomNavigator = LocalBottomSheetNavigator.current
+        val screenModel = navigator.rememberNavigatorScreenModel {
+            inject<RepositoryListScreenModel> { parametersOf(userName) }.value
+        }
+        val state by screenModel.state.collectAsState()
 
         Column(
             modifier = Modifier
@@ -39,7 +55,33 @@ data class RepositoryListScreen(
             ) {
                 Icon(imageVector = Icons.Default.Close, contentDescription = "close")
             }
-            Text(text = "test $userName")
+            // smart castがState.Successの中で効かないのであえて一旦currentに代入して比較している
+            // -> whenでのマッチしたあとでstateが変わっている可能性があるため
+            val current = state
+            when (current) {
+                RepositoryListScreenModel.State.Loading -> {
+                    CircularProgressIndicator()
+                }
+
+                is RepositoryListScreenModel.State.Success -> {
+                    LazyColumn {
+                        items(
+                            current.repositories.size,
+                            key = { current.repositories[it].hashCode() },
+                        ) {
+                            val item = current.repositories[it]
+                            Text(text = item.fullName)
+                        }
+                    }
+                }
+
+                is RepositoryListScreenModel.State.Error -> {
+                    Text(text = "Error: ${current.throwable}")
+                    Button(onClick = { screenModel.load() }) {
+                        Text(text = "retry")
+                    }
+                }
+            }
         }
     }
 }
